e42aa5cfca727bff3d3644f59fde8a54f4ae0f9c
Shashank Bharide <shashankbharide@cgi.com>
2024-09-14 22:30:20 -0400

    FINPLAY-7 Create Spring boot application

---
+++ src/main/java/com/finplay/mainapp/FinplayMainAppApplication.java
@@ -0,0 +1,13 @@
+package com.finplay.mainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinplayMainAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinplayMainAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finplay/mainapp/FinplayMainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finplay.mainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinplayMainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
