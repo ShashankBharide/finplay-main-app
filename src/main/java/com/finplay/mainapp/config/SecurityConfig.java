1e861991685d2cc76a61d0eceaeeeb10f7c86f70
Shashank Bharide <shashankbharide@cgi.com>
2024-11-13 22:59:26 -0500

    Added Security Config

--- src/main/java/com/finplay/mainapp/config/SecurityConfig.java
+++ src/main/java/com/finplay/mainapp/config/SecurityConfig.java
@@ -1,8 +1,28 @@
 package com.finplay.mainapp.config;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Arrays;
+
+@Configuration
 
 public class SecurityConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
 
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Angular dev server or any local frontend
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
 }
